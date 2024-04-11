be19eb7302fb03a25a2ce3f6964f6f4537965389
EricLima7 <ericalcg2@hotmail.com>
2024-04-11 19:26:12 -0300

    finalizado

---
+++ src/main/java/org/example/Exercicio3/Funcionario.java
@@ -0,0 +1,25 @@
+package org.example.Exercicio3;
+class Funcionario {
+    private String nome;
+    private double salarioBruto;
+    private double taxa;
+
+    public Funcionario(String nome, double salarioBruto, double taxa) {
+        this.nome = nome;
+        this.salarioBruto = salarioBruto;
+        this.taxa = taxa;
+    }
+
+    public double obterSalarioLiquido() {
+        return salarioBruto - (salarioBruto * taxa / 100);
+    }
+
+    public void aumentoSalario(double percentage) {
+        salarioBruto += salarioBruto * percentage / 100;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + ", Salário Bruto: " + salarioBruto + ", Salário Líquido: " + obterSalarioLiquido();
+    }
+}
--- src/main/java/org/example/Exercicio3/Main.java
+++ src/main/java/org/example/Exercicio3/Main.java
@@ -1,4 +1,21 @@
 package org.example.Exercicio3;
-
+import javax.swing.*;
 public class Main {
+    public static void main(String[] args) {
+        Funcionario funcionario1 = new Funcionario("João", 5000, 10);
+        Funcionario funcionario2 = new Funcionario("Maria", 6000, 15);
+
+        JOptionPane.showMessageDialog(null, funcionario1.toString());
+        JOptionPane.showMessageDialog(null, funcionario2.toString());
+
+        funcionario1.aumentoSalario(10);
+
+        JOptionPane.showMessageDialog(null, "Após o aumento salarial:");
+        JOptionPane.showMessageDialog(null, funcionario1.toString());
+        JOptionPane.showMessageDialog(null, funcionario2.toString());
+
+        JOptionPane.showMessageDialog(null, "Salários líquidos:");
+        JOptionPane.showMessageDialog(null, "Funcionário 1: " + funcionario1.obterSalarioLiquido());
+        JOptionPane.showMessageDialog(null, "Funcionário 2: " + funcionario2.obterSalarioLiquido());
+    }
 }
